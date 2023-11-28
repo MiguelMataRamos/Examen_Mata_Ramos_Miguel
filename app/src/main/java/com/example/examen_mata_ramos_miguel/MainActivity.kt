0712caf40ab2685d2d75ecab63e64bc24e7f59f8
EAG <mataramosmiguel@gmail.com>
2023-11-28 18:57:38 +0100

    comiiit

---
+++ app/src/main/java/com/example/examen_mata_ramos_miguel/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.examen_mata_ramos_miguel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
